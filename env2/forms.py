603f937ed46d2daf6d26e7a5ebbe9c4a036b333b
lap <sushen1990@163.com>
2018-11-14 17:31:06 +0800

    加入修改

--- env2/forms.py
+++ env2/forms.py
@@ -5,6 +5,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, SelectField, RadioField, \
     TextAreaField, DateField, BooleanField, FileField
 from wtforms.validators import DataRequired, ValidationError
+
 # from wtforms.validators import passwordValid
 
 class LoginForm(FlaskForm):
@@ -18,7 +19,7 @@ class LoginForm(FlaskForm):
     is_checked = BooleanField("是否验证通过")
     submit = SubmitField("登陆")
 
-class NewsForm(FlaskForm):
+class Form_news(FlaskForm):
     title = StringField(label='新闻标题' , validators=[DataRequired("请输入标题")] , description='请输入标题' ,
                         render_kw={"required":"required", "class":"form-control"}
                         )
--- env2/login.py
+++ env2/login.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for , flash , redirect , abort
 from NewsForm import News, Comment
-from forms import NewsForm
+from forms import Form_news
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 db = SQLAlchemy(app) # db是必须要引入的
@@ -17,10 +18,47 @@ def login1():
     news_list = News.query.all()
     return render_template("admin.html", news_list=news_list) # 注意这里form就是定义好的类
 
-@app.route("/add")
+@app.route("/add", methods=["GET","POST"])
 def add_news():
-    """新增页面"""
-    news = NewsForm()
+    """新增新闻"""
+    news = Form_news()
+
+    if news.is_submitted():
+        news_toadd = News(
+            title=news.title.data,
+            content=news.content.data,
+            news_type=news.news_type.data,
+            create_at=datetime.now()
+        )
+        db.session.add(news_toadd)
+        db.session.commit()
+        flash('保存成功')
+        return redirect(url_for("login1"))
+
     return render_template("add.html", news = news)
+
+@app.route("/add/update<int:pk>", methods=["GET","POST"])
+def update_news(pk):
+
+    from NewsForm import db as thisdb
+    """修改新闻"""
+    new_obj = News.query.get(pk)
+    if  new_obj is None:
+        abort(404)
+
+    news = Form_news(obj=new_obj)
+    if news.is_submitted():
+        new_obj.title = news.title.data
+        new_obj.content = news.content.data
+        new_obj.news_type = news.news_type.data
+        new_obj.update_at = datetime.now()
+        thisdb.session.add(new_obj)
+        thisdb.session.commit()
+        flash('修改成功')
+        return redirect(url_for("login1"))
+
+    return render_template("add.html", news = news)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
